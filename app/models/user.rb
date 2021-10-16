0ad1b7c8e7467d4be3f8e1cb83ab636d7392ff54
osmktok <o_k.2o11@icloud.com>
2021-10-16 17:22:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数を両方含む必要があります' }
+  validates :relationship_id, presence: true
 end
