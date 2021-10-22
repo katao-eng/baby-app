21ac5be2f5786a119d235c7bd68b2987f35ceeb1
osmktok <o_k.2o11@icloud.com>
2021-10-22 15:14:17 +0900

    ルーティングをネストに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "vaccination_lists#index"
-  resources :babies, only: [:index, :new, :create]
-  resources :vaccination_lists, only: [:index]
+  resources :babies, only: [:index, :new, :create] do
+    resources :vaccination_lists, only: [:index]
+  end
 end
