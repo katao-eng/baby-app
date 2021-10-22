4a29aa29c4125c86a7ae2dc6dbbaceec606b015f
osmktok <o_k.2o11@icloud.com>
2021-10-22 10:03:03 +0900

    vaccineモデル単体テストの実装

--- spec/models/vaccine_spec.rb
+++ spec/models/vaccine_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Vaccine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @vaccine = FactoryBot.create(:vaccine)
+  end
+
+  describe 'ワクチンの登録' do
+    context 'ワクチンの登録ができる場合' do
+      it 'name、kind_idが存在すれば登録できる' do
+        expect(@vaccine).to be_valid
+      end
+    end
+    context 'ワクチンの登録ができない場合' do
+      it 'nameがないと登録できない' do
+        @vaccine.name = ''
+        @vaccine.valid?
+        expect(@vaccine.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'kind_idがないと登録できない' do
+        @vaccine.kind_id = ''
+        @vaccine.valid?
+        expect(@vaccine.errors.full_messages).to include("Kind can't be blank")
+      end
+    end
+  end
 end
