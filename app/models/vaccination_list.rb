42a6c45984d864b521a1ceb181a41e1c6bdc65e6
osmktok <o_k.2o11@icloud.com>
2021-10-22 11:18:51 +0900

    バリデーションの設定

--- app/models/vaccination_list.rb
+++ app/models/vaccination_list.rb
@@ -1,4 +1,12 @@
 class VaccinationList < ApplicationRecord
   belongs_to :baby
   belongs_to :vaccine
+
+  with_options presence: true do
+    validates :start_date
+    validates :end_date
+    validates :status
+    validates :baby_id
+    validates :vaccine_id
+  end
 end
