4c075ef2f2d9d327fc9ec96f7b410180a9f62cd2
osmktok <o_k.2o11@icloud.com>
2021-10-30 08:14:49 +0900

    herokuデバッグ

--- db/migrate/20211028205339_remove_status_from_vaccination_lists.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromVaccinationLists < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :vaccination_lists, :status, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_205339) do
+ActiveRecord::Schema.define(version: 2021_10_22_020446) do
 
   create_table "babies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
