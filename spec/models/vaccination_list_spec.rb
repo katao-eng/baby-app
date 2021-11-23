ff939494bb1ae3a02034744615a4b011b1fcacf2
osmktok <o_k.2o11@icloud.com>
2021-11-23 16:58:25 +0900

    vaccination_listsモデルの単体テスト作成開始

--- spec/models/vaccination_list_spec.rb
+++ spec/models/vaccination_list_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe VaccinationList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    baby = FactoryBot.create(:baby)
+    vaccine = FactoryBot.create(:vaccine)
+    @vaccination_list = FactoryBot.build(:vaccination_list, baby_id: baby.id, vaccine_id: vaccine.id)
+  end
+
+  describe 'ワクチンリストの登録' do
+    context 'ワクチンリストの登録ができる場合' do
+      it 'date, start_date, end_dateがあれば登録できる' do
+        expect(@vaccination_list).to be_valid
+      end
+    end
+  end
 end
