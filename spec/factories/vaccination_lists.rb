1625b12416e67ac4022c4027ed3f3829f9bd8e19
osmktok <o_k.2o11@icloud.com>
2021-11-23 16:57:25 +0900

    ワクチンリストのFactoryBot作成

--- spec/factories/vaccination_lists.rb
+++ spec/factories/vaccination_lists.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :vaccination_list do
-    
+    date { Date.today }
+    start_date { Faker::Date.between(from: 1.year.ago, to: Date.today) }
+    end_date { Faker::Date.between(from: Date.today, to: 1.year.from_now) }
+    association :baby, :vaccine
   end
 end
